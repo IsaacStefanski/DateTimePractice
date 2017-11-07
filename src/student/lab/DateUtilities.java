a7fabd7d35161a84b9d01233bbc85221779a8741
Isaac <Isaac@ISAAC_HP_OMEN>
2017-11-07 14:57:19 -0600

    Work on DateUtilities in student.lab

--- src/student/lab/DateUtilities.java
+++ src/student/lab/DateUtilities.java
@@ -1,3 +1,4 @@
+//edu.wctc.advjava.ics.datetime.DateUtilities
 package student.lab;
 
 import java.time.Duration;
@@ -9,26 +10,33 @@ import java.time.temporal.ChronoUnit;
 import javax.swing.text.DateFormatter;
 
 /**
- *
- * @author Isaac
+ * This is a utility class containing different methods that utilize the newer
+ * <b>Java 8<b> Date and Time API. These methods can be used as tools to do
+ * various calculations with times and dates.
+ * 
+ * @author Isaac Stefanski, istefanski@my.wctc.edu
+ * @version 1.00
+ * @since 1.8
  */
 public class DateUtilities {
+    
     /**
-     * @return the current date as yyyy-mm-dd.
+     * @return current date with the default yyyy-mm-dd format.
      */
     public final LocalDate today(){
         return LocalDate.now();
     }
     
     /**
-     * @return the current date and time as yyyy-mm-ddThh:mm:ss.
+     * @return current date and time with the default yyyy-mm-ddThh:mm:ss format.
      */
     public final LocalDateTime now(){
         return LocalDateTime.now();
     }
     
     /**
-     * Format a <code>LocalDate</code> according to the pattern passed in
+     * Format a <code>LocalDate</code> according to the pattern passed in by
+     * the user.
      *
      * @param date - a <code>LocalDate</code> object
      * @param pattern - a <code>String</code> object
@@ -45,6 +53,7 @@ public class DateUtilities {
     
     /**
      * Format a <code>LocalDateTime</code> according to the pattern passed in
+     * by the user.
      *
      * @param dateTime - a <code>LocalDateTime</code> object
      * @param pattern - a <code>String</code> object
@@ -60,12 +69,15 @@ public class DateUtilities {
     }
     
     /**
-     * Format a <code>LocalDate</code> from a <code>String</code>
+     * Format a <code>LocalDate</code> from a <code>String</code> passed in by
+     * the user.
      *
      * @param inputDate - a <code>LocalDate</code> object
      * @param pattern - a <code>String</code> object
-     * @return a date formatted according to the date pattern passed in
-     * @throws IllegalArgumentException if date string is null
+     * @return a <code>LocalDate</code> formatted according to the date pattern
+     * passed in by the user
+     * @throws IllegalArgumentException if date string is null or does not
+     * contain valid <code>LocalDate</code> content
      */
     public final LocalDate toLocalDate(String inputDate, String pattern) throws IllegalArgumentException{
         if(inputDate == null || inputDate.isEmpty() || inputDate.equals("-") || inputDate.equals(" ")){
@@ -76,22 +88,27 @@ public class DateUtilities {
     }
     
     /**
-     * Format a <code>LocalDateTime</code> from a <code>String</code>
+     * Format a <code>LocalDateTime</code> from a <code>String</code>.
      *
      * @param inputDateTime - a <code>LocalDate</code> object
      * @param pattern - a <code>String</code> object
-     * @return a date formatted according to the date pattern passed in
-     * @throws IllegalArgumentException if date string is null
+     * @return a <code>LocalDateTime</code> formatted according to the pattern
+     * passed in by the user
+     * @throws IllegalArgumentException if date string is null or does not
+     * contain valid <code>LocalDate</code> content
      */
     public final LocalDateTime toLocalDateTime(String inputDateTime, String pattern) throws IllegalArgumentException{
-        if(inputDateTime == null || inputDateTime.isEmpty() || inputDateTime.equals("-") || inputDateTime.equals(" ")){
-            throw new IllegalArgumentException("Input date string is invalid");
+        if(inputDateTime == null || inputDateTime.isEmpty() || inputDateTime.equals("-")
+                || inputDateTime.equals(" ") || !inputDateTime.contains(":")){
+            throw new IllegalArgumentException("Input date and time string is invalid");
         }
         LocalDateTime localDateTime = LocalDateTime.parse(inputDateTime, DateTimeFormatter.ofPattern(pattern));
         return localDateTime;
     }
     
     /**
+     * Calculate a <code>Duration</code> between a start date and end date.
+     * 
      * @param startDate - a <code>LocalDate</code> object
      * @param endDate - a <code>LocalDate</code> object
      * @return duration between start and end dates
@@ -106,24 +123,30 @@ public class DateUtilities {
     }
     
     /**
-     * @param startDate - a <code>LocalDate</code> object
-     * @param endDate - a <code>LocalDate</code> object
-     * @return duration in hours between start and end dates
-     * @throws IllegalArgumentException if any date is null
+     * Calculate the number of hours between two times.
+     * 
+     * @param startDate - a <code>LocalDateTime</code> object
+     * @param endDate - a <code>LocalDateTime</code> object
+     * @return a <code>long</code> with the value of the calculated duration in
+     * hours between start and end times
+     * @throws IllegalArgumentException if any <code>LocalDateTime</code>
+     * instance passed in is null
      */
-    public final long findHoursBetween(LocalDate startDate, LocalDate endDate) throws IllegalArgumentException{
-        if(startDate == null || endDate == null){
+    public final long findHoursBetween(LocalDateTime startDateTime, LocalDateTime endDateTime) throws IllegalArgumentException{
+        if(startDateTime == null || endDateTime == null){
             throw new IllegalArgumentException("Start and end dates cannot be null");
         }
-        Duration diff = Duration.between(startDate, endDate);
+        Duration diff = Duration.between(startDateTime, endDateTime);
         long hours = diff.toHours();
         return hours;
     }
     
     /**
+     * Calculate the number of days between two dates specified by the user.
+     * 
      * @param startDate - a <code>LocalDate</code> object
      * @param endDate - a <code>LocalDate</code> object
-     * @return duration in hours between start and end dates
+     * @return duration in days between start and end dates
      * @throws IllegalArgumentException if any date is null
      */
     public final long findDaysBetween(LocalDate startDate, LocalDate endDate) throws IllegalArgumentException{
